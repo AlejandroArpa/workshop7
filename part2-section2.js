8ca204aa81a981fbbe0313bf5350974a01634723
Alejandro Ramirez <alejorp195@hotmail.com>
2024-05-14 13:48:19 -0500

    part2-section2 Done

--- part2-section2.js
+++ part2-section2.js
@@ -29,6 +29,10 @@ User.prototype.acomularPuntos = ( points ) => {
     this.pointsStock += points 
 }
 
+User.prototype.canjearPuntos = ( points ) => {
+    this.pointsStock -= points 
+}
+
 function Admin(name, password, email, id){
     Person.call(this, name, password, email, id)
 }
@@ -48,3 +52,76 @@ Admin.prototype.eliminarProducto = ( product ) => {
     // deleteProduct(product)
 }
 
+function Product ( name, id, puntosNecesarios, cantidadDisponible, stock, categoria_id ){
+    this.name = name
+    this.id = id
+    this.puntosNecesarios = puntosNecesarios
+    this.cantidadDisponible = cantidadDisponible
+    this.stock = stock
+    this.categoria_id = categoria_id
+}
+
+function DigitalProduct ( name, id, puntosNecesarios, cantidadDisponible, stock, categoria_id, url ){
+    Product.call( this, name, id, puntosNecesarios, cantidadDisponible, stock, categoria_id )
+    this.url = url
+}
+
+DigitalProduct.prototype = Object.create( Product.prototype );
+DigitalProduct.prototype.constructor = DigitalProduct;
+
+DigitalProduct.prototype.descargar = () => {
+
+}
+
+DigitalProduct.prototype.obtenerProductEmail = () => {
+
+}
+
+function PhysicalProduct ( name, id, puntosNecesarios, cantidadDisponible, stock, categoria_id, precio ){
+    Product.call( this, name, id, puntosNecesarios, cantidadDisponible, stock, categoria_id )
+    this.precio = precio
+}
+
+PhysicalProduct.prototype = Object.create( Product.prototype );
+PhysicalProduct.prototype.constructor = PhysicalProduct;
+
+PhysicalProduct.prototype.actualizarStock = () =>{
+
+}
+
+PhysicalProduct.prototype.enviarProducto = () => {
+
+}
+
+function Activity ( type, pointsGiven ){
+    this.type = type;
+    this.pointsGiven = pointsGiven;
+}
+
+Activity.prototype.completeActivity = () =>{
+
+}
+
+function Categories ( name, id, description ){
+    this.name = name;
+    this.id = id;
+    this.description = description;
+}
+
+Categories.prototype.listProducts = () => {
+
+}
+
+function ExchangeOrder ( user_id, product_id, date ){
+    this.user_id = user_id;
+    this.product_id = product_id;
+    this.date = date;
+}
+
+ExchangeOrder.prototype.confirmOrder = () => {
+
+}
+
+ExchangeOrder.prototype.cancelOrder = () => {
+    
+}
